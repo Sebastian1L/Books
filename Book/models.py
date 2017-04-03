6de6a5bca1d7c30eebadbcd89bfaf667568d90c1
Sebastian Lubanski <sebastian.lubanski22@gmail.com>
2017-04-04 01:43:25 +0200

    Added models: Authot, Pulisher and Book in Book app. Added Book app in settings.py

--- Book/models.py
+++ Book/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=50)
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=70)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author)
+    isbn = models.CharField(max_length=17)
+    publisher = models.ForeignKey(Publisher)
+
+
